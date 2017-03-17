9df3170f3c450c77520fc52140c01f716413a5ed
robinvh85 <hoangviet.vh@gmail.com>
2017-03-17 18:25:26 +0700

    Complete 00, 01 Doing 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+
+def ftoc(f_tem)
+	(f_tem - 32) * 5 / 9
+end
+
+def ctof(c_tem)
+	(c_tem * 9 / 5.0) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
-#write your code here
+
+def add(num1, num2)
+	num1 + num2
+end
+
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def sum(nums)
+	result = 0
+
+	nums.each { |num| result += num }
+	result
+end
+
+def multiply(nums)
+	result = 1
+
+	nums.each { |num| result *= num }
+	result
+end
+
+def power(num1, num2)
+	num1 ^ num2
+end
